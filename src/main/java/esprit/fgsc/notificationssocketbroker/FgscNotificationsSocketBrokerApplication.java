f2e4fec839374f91c06ba7bce9bdd473c98c6aaa
chihabhajji <blackpang23@gmail.com>
2021-10-25 00:21:42 +0100

    forgot to add EurekaClient annotationdumb dumb

--- src/main/java/esprit/fgsc/notificationssocketbroker/FgscNotificationsSocketBrokerApplication.java
+++ src/main/java/esprit/fgsc/notificationssocketbroker/FgscNotificationsSocketBrokerApplication.java
@@ -2,8 +2,17 @@ package esprit.fgsc.notificationssocketbroker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
-
+@EnableWebFlux
+@RestController
+@CrossOrigin("*")
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class FgscNotificationsSocketBrokerApplication {
     public static void main(String[] args) {
