c8c2c2ce765662ef01e28bda404b775a51eb347f
chihabhajji <blackpang23@gmail.com>
2021-10-27 18:27:46 +0100

    Swagger Docs

--- src/main/java/esprit/fgsc/notificationssocketbroker/FgscNotificationsSocketBrokerApplication.java
+++ src/main/java/esprit/fgsc/notificationssocketbroker/FgscNotificationsSocketBrokerApplication.java
@@ -5,11 +5,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Slf4j
 @EnableEurekaClient
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableSwagger2
+@FeignClient(name = "notifs")
+@RibbonClient(name = "notifs")
 public class FgscNotificationsSocketBrokerApplication {
     public static void main(String[] args) {
         SpringApplication.run(FgscNotificationsSocketBrokerApplication.class, args);
---
+++ src/main/java/esprit/fgsc/notificationssocketbroker/config/SwaggerConfig.java
@@ -0,0 +1,55 @@
+package esprit.fgsc.notificationssocketbroker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.*;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        Docket d = new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+        d.apiInfo(metadata());
+        return d;
+    }
+    public static ApiInfo metadata(){
+        return new ApiInfoBuilder()
+                .title("FGSC customer feedback acquisition microservice")
+                .description("A microservice for managing customer feedback acquisition campagins with FormioJs")
+                .contact(new Contact("Chihab hajji", "https://github.com/chihabhajji","chihab.hajji@esprit.tn"))
+                .version("0.1")
+                .build();
+    }
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .showExtensions(false)
+                .showCommonExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+                .validatorUrl(null)
+                .build();
+    }
+}
